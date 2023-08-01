10e51f9f0de86233ab7b8c938efd19790875fe80
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2023-08-02 00:57:48 +0530

    Update macchanger.sh

--- macchanger.sh
+++ macchanger.sh
@@ -61,7 +61,7 @@ select interface in "${interfaces[@]}"; do
     git clone  https://github.com/strangedreamer4/MacChanger.git
     cd MacChanger
     chmod +x macchanger.sh
-    ./macchanger.sh
+    
     break
   else
     espeak " Invalid option "
