e8617285b84ae1151f6c1c35f656173fe2c93b7f
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2023-09-30 18:07:53 +0530

    Update machanger.sh

--- machanger.sh
+++ machanger.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Install espeak silently
-sudo apt install espeak -y > /dev/null 2>&1
-# Additional code snippet
-# ALSA lib pcm.c errors
-sudo alsa force-reload
-killall jackd
-clear
-
 # Function to display the colorful loading effect
 show_loading() {
   local duration="$1"
@@ -22,26 +14,6 @@ show_loading() {
     sleep "$interval"
   done
 }
-
-# Usage example: Display the loading effect for 5 seconds
-show_loading 5
-echo "Loading complete!"
-sleep 2
-clear
-
-echo "                                          "
-echo "                 .o888P      Y8o8Y      Y888o. "
-echo "                d88888       88888      88888b "
-echo "                d888888b_  _d88888b_  _d888888b "
-echo "                8888888888888888888888888888888 "
-echo "                8888888888888888888888888888888 "
-echo "                YJGS8P'Y888P'Y888P'Y888P'Y8888P "
-echo "                 Y888   '8'   Y8P   '8'   888Y "
-echo "                  '8o          V          o8' "
-echo "    \`                     \`"
-echo "              Welcome to MacChanger -\$ "
-echo "               by StRaNgErDreAmEr "
-espeak "welcome user"
 
 # Function to change MAC address for the specified network interface
 change_mac_address() {
@@ -61,19 +33,80 @@ change_mac_address() {
   sudo ip link set "$interface" up
 }
 
-# Get a list of available network interfaces
-interfaces=($(ip -o link show | awk -F': ' '{print $2}'))
+# Display a welcome message
+clear
+echo "                                          "
+echo "                 .o888P      Y8o8Y      Y888o. "
+echo "                d88888       88888      88888b "
+echo "                d888888b_  _d88888b_  _d888888b "
+echo "                8888888888888888888888888888888 "
+echo "                8888888888888888888888888888888 "
+echo "                YJGS8P'Y888P'Y888P'Y888P'Y8888P "
+echo "                 Y888   '8'   Y8P   '8'   888Y "
+echo "                  '8o          V          o8' "
+echo "    \`                     \`"
+echo "              Welcome to MacChanger -\$ "
+echo "               by StRaNgErDreAmEr "
+espeak "Welcome user"
 
-# Display a menu for selecting the network interface
-PS3="Enter the number corresponding to the network interface you want to change the MAC address for: "
-select interface in "${interfaces[@]}"; do
-  if [[ -n "$interface" ]]; then
-    change_mac_address "$interface"
-    sleep 5
-    git pull
-  else
-    espeak "Invalid option"
-    echo "Invalid option. Please select a valid number."
+# Install espeak silently
+sudo apt install espeak -y > /dev/null 2>&1
+
+# Additional code snippet
+# ALSA lib pcm.c errors
+sudo alsa force-reload
+killall jackd
+
+while true; do
+  # Check for local changes in machanger.sh
+  if [[ $(git diff machanger.sh) ]]; then
+    # Local changes found
+    echo "Local changes detected in machanger.sh."
+    echo "Do you want to commit, stash your local changes, or exit? (commit/stash/exit)"
+    read choice
+
+    case "$choice" in
+      "commit")
+        # Commit your local changes
+        git add machanger.sh
+        git commit -m "Committing local changes to machanger.sh"
+        ;;
+      "stash")
+        # Stash your local changes
+        git stash
+        ;;
+      "exit")
+        echo "Exiting the script."
+        exit
+        ;;
+      *)
+        echo "Skipping update due to local changes."
+        ;;
+    esac
   fi
+
+  # Perform the pull to update the script
+  git pull
+
+  # Get a list of available network interfaces
+  interfaces=($(ip -o link show | awk -F': ' '{print $2}'))
+
+  # Display a menu for selecting the network interface or exiting
+  PS3="Enter the number corresponding to the network interface you want to change the MAC address for (or '0' to exit): "
+  select interface in "${interfaces[@]}"; do
+    if [[ "$REPLY" == "0" ]]; then
+      echo "Exiting the script."
+      exit
+    elif [[ -n "$interface" ]]; then
+      change_mac_address "$interface"
+      sleep 5
+      break
+    else
+      espeak "Invalid option"
+      echo "Invalid option. Please select a valid number or '0' to exit."
+    fi
+  done
 done
 
+# Add an exit message here (this will only be reached if the user exits the script)
+echo "Script finished running. Exiting."
